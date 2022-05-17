5733765cfa463f77ea155bd0a4626e95ed8e803c
Jeongmin Kim <kjmgi0671@naver.com>
2022-05-17 19:59:05 +0900

    2022 05-17
    prototype

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+struct TitleBtn
+{
+
+}
+
+public class TitleManager : MonoBehaviour
+{
+    private static TitleManager instance;
+    public static TitleManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+
+    [SerializeField] private GameObject TitleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartTitle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator StartTitle()
+    {
+
+
+
+        yield return null;
+    }
+}
